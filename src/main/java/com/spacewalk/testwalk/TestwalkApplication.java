415fdcdf7f738f9e3d7c3b28ff364da033041e00
gorockgorock <hsyjjk@gmail.com>
2024-06-03 22:23:19 +0900

    init commit

---
+++ src/main/java/com/spacewalk/testwalk/TestwalkApplication.java
@@ -0,0 +1,13 @@
+package com.spacewalk.testwalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestwalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestwalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spacewalk/testwalk/TestwalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacewalk.testwalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestwalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
